600a4a295dec62d36a0f9fd040942e9faee4b863
Gerwandus <127389431+Pawel2755@users.noreply.github.com>
2026-02-27 09:27:13 +0100

    27luty

---
+++ 27luty/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    /*Zad 1 Napisz program, który prosi u¿ytkownika o wprowadzenie jednej liczby
+    ca³kowitej, a nastêpnie wyœwietla j¹ na ekranie.*/
+
+    /*int liczba;
+    printf("Podaj liczbe: ");
+    scanf("%d",&liczba);
+    printf("%d",liczba);*/
+
+    /*Zad 2 Stwórz program, który wczytuje od u¿ytkownika dwie liczby
+    zmiennoprzecinkowe i wypisuje ich ró¿nicê.
+
+    double liczba1;
+    double liczba2;
+    printf("Podaj dwie liczby: ");
+    scanf("%lf %lf",&liczba1,&liczba2);
+    printf("%lf",liczba1 - liczba2);*/
+
+    /* Zad 3 Zaprojektuj aplikacjê, która pyta
+    u¿ytkownika o jego rok urodzenia, a nastêpnie
+    wypisuje rok poprzedni (o jeden mniejszy).
+
+    int liczba;
+    printf("Podaj rok urodzenia: ");
+    scanf("%d",&liczba);
+    printf("%d",liczba - 1);*/
+
+    /*Zad 4 Napisz program, który wczytuje od u¿ytkownika trzy
+    liczby ca³kowite i wypisuje ich œredni¹ jako wartoœæ zmiennoprzecinkow¹
+
+    int liczba1;
+    int liczba2;
+    int liczba3;
+    printf("Podaj 3 liczby: ");
+    scanf("%d %d %d",&liczba1,&liczba2,&liczba3);
+    printf("%f",(float)(liczba1+liczba2+liczba3)/3);*/
+
+    /* Zad 5 Utwórz program, który prosi u¿ytkownika o
+    wprowadzenie dwóch liter (znaków),
+    a nastêpnie wypisuje je w odwrotnej kolejnoœci.
+
+    char znak1[2];
+    char znak2[2];
+    printf("Podaj 1 litere: ");
+    scanf("%s",znak1);
+    printf("Podaj 2 litere: ");
+    scanf("%s",znak2);
+    printf("%s",znak2);
+    printf("%s",znak1);*/
+
+    /*zad 6 Napisz program, który prosi u¿ytkownika o
+    wprowadzenie jednej liczby zmiennoprzecinkowej,
+    a nastêpnie podwaja jej wartoœæ i wypisuje wynik.
+
+    double liczba;
+    printf("Podaj liczbe: ");
+    scanf("%lf",&liczba);
+    printf("%lf",liczba*2);*/
+
+//    Zad 7 Utwórz program, który wczytuje od u¿ytkownika liczbê
+//    zmiennoprzecinkow¹ reprezentuj¹c¹ kwotê w dolarach, a nastêpnie wypisuje,
+//    ile to jest euro, przyjmuj¹c sta³y kurs wymiany (np. 1 dolar = 0.85 euro).
+
+
+
+
+
+    /*int liczbacalkowita = 125;
+    int liczba2 = 45;
+    int liczba3;
+
+
+    char napis[25];
+    printf("Podaj napis (max 24 znaki): ");
+    scanf("%24s",napis);
+    printf("%s",napis);
+
+
+    printf("%d\n",&liczba3);
+    printf("Podaj liczbe:");
+    scanf("%d",&liczba3);znak "&" zeby dzialalo poprawnie
+    printf("liczby: %d druga liczba %d, %d",liczbacalkowita, liczba2,liczba3);*/
+    return 0;
+}
