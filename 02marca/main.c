ddf959d33f20ab4d145a5c183997e6a39d70c2a7
Gerwandus <127389431+Pawel2755@users.noreply.github.com>
2026-03-02 11:11:41 +0100

    02marca

---
+++ 02marca/main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(void)
+{
+
+//        1Napisz program, który oblicza resztê z dzielenia sumy dwóch
+//        liczb ca³kowitych przez trzeci¹ liczbê ca³kowit¹.
+//        int a = 6;
+//        int b = 2;
+//        int c = 3;
+//
+//        double reszta = (a+b) % c;
+//        printf("%lf",reszta);
+
+//        2Stwórz program, który oblicza ró¿nicê kwadratów dwóch
+//        podanych liczb ca³kowitych.
+//        int a = 3;
+//        int b = 5;
+//        int wynik = (3*3)-(5*5);
+//        printf("%d",wynik);
+//
+//        3Opracuj program, który oblicza iloczyn ró¿nicy
+//        dwóch liczb ca³kowitych i trzeciej liczby ca³kowitej.
+//        int a = 5;
+//        int b = 7;
+//        int c = 6;
+//
+//        int wynik = (a-b)*c;
+//        printf("%d",wynik);
+
+
+//        4Zaprojektuj program, który oblicza œredni¹ geometryczn¹
+//        bezwzglêdnych wartoœci trzech podanych liczb ca³kowitych.
+//        int a = 3;
+//        int b = 5;
+//        int c = 7;
+//
+//        double wynik = pow((a*b*c),(1.0/3.0));
+//        printf("%lf\n", wynik);
+
+
+//        5Napisz program, który oblicza kwadrat sumy dwóch
+//        podanych liczb ca³kowitych.
+//        int a = 6;
+//        int b = 7;
+//        int wynik = pow(a+b,2);
+//        printf("%d\n", wynik);
+
+//        6Stwórz program, który oblicza sumê kwadratów
+//        trzech podanych liczb ca³kowitych.
+//        int a = 4;
+//        int b = 7;
+//        int c = 8;
+//        int wynik = (a*a)+(b*b)+(c*c);
+//        printf("%d\n", wynik);
+
+//        7Opracuj program, który oblicza, ile razy jedna podana liczba
+//        ca³kowita mieœci siê w drugiej podanej liczbie ca³kowitej.
+//        int a = 9;
+//        int b = 4;
+//        int wynik = a/b;
+//        printf("podana liczba miesci sie: %d razy",wynik);
+
+//        8Zaprojektuj program, który oblicza kwadrat ró¿nicy dwóch
+//        podanych liczb ca³kowitych.
+//        int a = 7;
+//        int b = 9;
+//        int wynik = (a*a)-(b*b);
+//        printf("%d\n", wynik);
+
+//          9Napisz program, który oblicza iloraz sumy dwóch liczb
+//          ca³kowitych przez ich ró¿nicê.
+//          int a = 7;
+//          int b = 4;
+//          int wynik = pow((a-b),2);
+//          printf("%d\n",wynik);
+
+//          10Stwórz program, który oblicza sumê trzech
+//          kolejnych liczb ca³kowitych, zaczynaj¹c od podanej liczby ca³kowitej.
+//          int a = 4;
+//          int b = 5;
+//          int c = 6;
+//          int wynik = a+b+c;
+//          printf("%d", wynik);
+
+
+
+
+
+
+
+//      double x1 = 0.1 + 0.2;
+//      double x2 = 0.3;
+//      double epsilon = 1e-9;
+//      if(fabs(x1 - x2) < epsilon){
+//        printf("prawidlowy wynik\n");
+//      }else if(fabs(x1 - x2) == epsilon){
+//        printf("roznica jest rowna dokladnie 0.000000001\n");
+//      }else{
+//        printf("nieprawidlowy wynik\n");
+//      }
+
+//      && - logiczny and
+//      || - logiczny or
+
+//    int a = 1;
+//    int b = 2;
+//    double c = 3.5;
+//    double d = 3.2;
+//
+//    a = a + b;
+//    a += b;
+//
+//    a = a - b;
+//    a -= b;
+//
+//    a = a * b;
+//    a *= b;
+//
+//    a = a / b;
+//    a /= b;
+//
+//    a = a % b;
+//    a %= b;
+//
+//    int i = 0;
+//    printf("%d ",i++);
+//    printf("%d ",i);
+//    printf("%d ",++i);
+//    printf("%d\n",i);
+//
+//
+//    double s = 0.1 + 0.2;
+//    printf("0.1 + 0.2 = %.20lf\n", s);
+//
+//    int x1 = 8;
+//    int x2 = 3;
+//    double x3 = (double)x1 / x2;
+//    printf("%lf\n", x3);
+
+    return 0;
+}
