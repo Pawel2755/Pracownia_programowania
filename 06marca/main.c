0475c11dacb3121563078f04a898d33170b26bcc
Gerwandus <127389431+Pawel2755@users.noreply.github.com>
2026-03-06 09:27:18 +0100

    06marca

---
+++ 06marca/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+//    int a = 6, b = 3; //a = 0b0110, b =0b0011
+//    printf("%d\n", a & b); // AND - 0b0010 = 2
+//    printf("%d\n", a | b); // OR - 0b0111 = 7
+//    printf("%d\n", a ^ b); // XOR - 0b0101 = 5
+//    printf("%d\n", ~a); // NOT - 0b1111111111111..1001 = -7
+//    printf("%d\n", a << 2); // LEFT SHIFT - 0b011000 = 24
+//    printf("%d\n", a >> 2); // RIGHT SHIFT - 0b0001 = 1
+
+
+//    1.Zamieñ wartoœci dwóch zmiennych ca³kowitych
+//    bez u¿ycia dodatkowej zmiennej.
+
+//    int a = 6,b = 3;
+//    a = a ^ b;
+//    b = b ^ a;
+//    a = a ^ b;
+//    printf("%d %d",a, b);
+
+//    2.SprawdŸ, czy liczba ca³kowita jest parzysta czy nieparzysta
+//    za pomoc¹ operatorów bitowych.
+
+
+//    3.Wyznacz wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
+
+
+
+
+    return 0;
+}
