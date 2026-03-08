6951877a84589715a88e74be65540127734eb1ae
Gerwandus <127389431+Pawel2755@users.noreply.github.com>
2026-03-08 21:26:38 +0100

    operatory_bitowe

---
+++ operatory_bitowe/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    1.Zamien wartoœci dwóch zmiennych ca³kowitych
+//    bez u¿ycia dodatkowej zmiennej.
+
+//    int a = 6,b = 3;
+//    a = a ^ b;
+//    b = b ^ a;
+//    a = a ^ b;
+//    printf("%d %d",a, b);
+
+//    2.Sprawdz, czy liczba calkowita jest parzysta czy nieparzysta
+//    za pomoca operatorów bitowych.
+
+//    int a;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    if((a&1) == 0){
+//        printf("parzysta");
+//    }else{
+//        printf("nieparzysta");
+//    }
+
+//    3.Wyznacz wartosc bitu na okreslonej pozycji w liczbie calkowitej.
+
+//    int a,b;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    printf("Podaj pozycje: ");
+//    scanf("%d",&b);
+//    int wartosc = (a>>b) &1;
+//    printf("%d",wartosc);
+
+//    4.Zeruj wartość bitu na określonej pozycji w liczbie całkowitej.
+
+//    int a,b;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    printf("Podaj pozycje: ");
+//    scanf("%d",&b);
+//    a = a & ~(1<<b);
+//    printf("%a",a);
+
+//    5.Odwróć wartość wszystkich bitów w liczbie całkowitej.
+
+
+//    int a;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    printf("%d",~a);
+
+//    6.Przesuń bity liczby całkowitej o określoną liczbę pozycji w lewo.
+
+//    int a,b;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    printf("Podaj liczbe pozycji");
+//    scanf("%d",&b);
+//    printf("%d",a<<b);
+
+
+//    7.Przesuń bity liczby całkowitej o określoną liczbę pozycji w prawo.
+
+//    int a,b;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    printf("Podaj liczbe pozycji");
+//    scanf("%d",&b);
+//    printf("%d",a>>b);
+
+//    8.Wyznacz “XOR” dwóch liczb całkowitych.
+
+//    int a,b;
+//    printf("Podaj liczby: ");
+//    scanf("%d %d",&a, &b);
+//    printf("Wynik: %d",a^b);
+
+    return 0;
+}
