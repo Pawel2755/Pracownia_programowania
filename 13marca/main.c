8de7ee3b1f70466ffcd1bda5be16c0347d46ba96
Gerwandus <127389431+Pawel2755@users.noreply.github.com>
2026-03-13 09:19:55 +0100

    13marca

---
+++ 13marca/main.c
@@ -0,0 +1,127 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+//    1.Napisz program, który wyœwietla wszystkie liczby ca³kowite od 1 do 100, u¿ywaj¹c pêtli for
+//    for(int i = 1 ;i <= 100; i++){
+//        printf("%d\n",i);
+//    }
+
+
+//    Zad.2
+
+//    int n,a=1,b;
+//    printf("Podaj liczbe calkowita: ");
+//    scanf("%d",&n);
+//    while(a<=n){
+//        b+=a;
+//        a++;
+//    }
+//    printf("%d",b);
+//    3.Napisz program, który czyta od u¿ytkownika liczby ca³kowite do momentu
+//    wprowadzenia zera i nastêpnie wyœwietla sumê wszystkich wprowadzonych liczb dodatnich
+//    oraz sumê wszystkich liczb ujemnych (osobno), u¿ywaj¹c pêtli do-while.
+
+//    int a,suma1,suma2;
+//    do{
+//        printf("Podaj liczbe: ");
+//        scanf("%d",&a);
+//        if(a>0){
+//            suma1 +=a;
+//        }else{
+//            suma2 +=a;
+//        }
+//    }while(a!=0);
+//
+//    printf("Suma liczb dodatnich: %d\nSuma liczb ujemnych: %d",suma1, suma2);
+
+
+//    4.Stwórz program, który oblicza i wyœwietla silniê podanej przez u¿ytkownika nieujemnej
+//    liczby ca³kowitej, u¿ywaj¹c pêtli for.
+
+//    int a,b=1;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&a);
+//    for(int i=1;i<=a;i++){
+//        b *= i;
+//    }
+//    printf("Silnia z liczby %d to: %d",a,b);
+
+//    5.Napisz program, który wyœwietla pierwszych 10 liczb ci¹gu Fibonacciego, u¿ywaj¹c pêtli while.
+
+//    int f1 = 0,f2 = 1,fx,b=0;
+//    printf("10 pierwszych wyrazow ciagu to:\n");
+//    while(b<=10){
+//        printf("%d",f1);
+//        printf("\n");
+//        fx=f1+f2;
+//        f1 = f2;
+//        f2 = fx;
+//        b++;
+//    }
+
+//    Zad.6
+
+//    int n,wynik;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&n);
+//    while(wynik*wynik<=n){
+//        wynik++;
+//    }
+//    wynik--;
+//    printf("%d",wynik);
+
+
+//    zad.7
+
+//    int n,wynik;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&n);
+//    while(wynik*wynik<=n){
+//        wynik++;
+//    }
+//    printf("%d",wynik);
+
+//    Zad.8
+
+//    int wynik;
+//    int a[10];
+//    printf("Podaj 10 liczb: ");
+//    for(int i=0;i<10;i++){
+//        scanf("%d",&a[i]);
+//    }
+//
+//    for (int l=1;l<9;l++){
+//        if(a[l]<((a[l-1]+a[l+1])/2)){
+//           wynik++;
+//           }
+//    }
+//
+//    printf("%d",wynik);
+
+//    Zad.9
+
+//    int wynik,n;
+//    printf("Podaj liczbe: ");
+//    scanf("%d",&n);
+//    int a[n];
+//    printf("Podaj liczby do ciagu: \n");
+//    for(int i=0;i<n;i++){
+//        printf("a%d: ",i+1);
+//        scanf("%d",&a[i]);
+//    }
+//
+//    for(int k=1;k<n-1;k++){
+//        if(a[k]<((a[k-1]+a[k+1])/2)){
+//           wynik++;
+//           }
+//    }
+//
+//    printf("%d",wynik);
+
+
+    return 0;
+}
